c34f77ae98117613b240dd42c37c9ce8d0d63936
Majidshah83 <shahmajid508@gmail.com>
2022-11-15 22:29:24 +0500

    create RegisterRequest for validation

--- app/Http/Controllers/RegisterControler.php
+++ app/Http/Controllers/RegisterControler.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\RegisterRequest;
 class RegisterControler extends Controller
 {
     public function register()
     {
         return view('layouts.register');
+    }
+    public function registerUser(RegisterRequest $request)
+    {
+        
     }
 }
