7a6c3ce0608b587ed72d3da99853c492c27a091a
Majidshah83 <shahmajid508@gmail.com>
2022-11-15 22:20:28 +0500

    create register page and RegisterController

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LoginController;
+use App\Http\Controllers\RegisterControler;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,3 +14,4 @@ use App\Http\Controllers\LoginController;
 |
 */
 route::get('login',[LoginController::class,'login'])->name('login');
+route::get('register',[RegisterControler::class,'register'])->name('register');
