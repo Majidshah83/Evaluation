58c16477b598b0f7cf45c7bce2282faf09077a5f
Majidshah83 <shahmajid508@gmail.com>
2022-11-15 22:09:19 +0500

    create login page and Logincontroller

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\LoginController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -12,7 +12,4 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+route::get('login',[LoginController::class,'login'])->name('login');
